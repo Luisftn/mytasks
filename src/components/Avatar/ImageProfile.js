2d1d33f956da2ca26eff8b0369e2f12c99f3d47e
Luis <luisft128@gmail.com>
2022-05-22 20:47:43 -0300

    Criação do avatar

---
+++ src/components/Avatar/ImageProfile.js
@@ -0,0 +1,12 @@
+import "./imageprofile.sass";
+
+function ImageProfile(){
+    return(
+        
+        <header className="container">
+            <h1 className="profile"></h1>
+        </header>
+    );
+}
+
+export default ImageProfile;
